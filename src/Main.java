32e8b0fa5fc29e0f5743f1b1adb13185d06a3dce
egolfc <cae5511@gmail.com>
2021-09-27 09:58:34 -0400

    initial commit. minimal work

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Tokenizer tokenizer = new Tokenizer();
+        Scanner s = new Scanner(args[1]);
+
+        
+    }
+}
---
+++ src/Tokenizer.java
@@ -0,0 +1,19 @@
+
+public class Tokenizer {
+
+    public static String getToken(String line){
+        return "token";
+    }
+
+    public static void skipToken(){
+
+    }
+
+    public static int intVal(String token){
+        return 1;
+    }
+
+    public static int idName(String token){
+        return 1;
+    }
+}
