dab83bc7a24ff46095b0a4844403c4555e9b27e5
egolfc <cae5511@gmail.com>
2021-09-29 13:30:35 -0400

    laptop battery not charging, trying different laptop

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
 public class Main {
     public static void main(String[] args) throws Exception {
-        // Tokenizer tokenizer = new Tokenizer();
-        Scanner s = new Scanner("C:/Christopher/School/OSU/AU21/CSE3341/Project/Tokenizer/src/test.txt");
-        // System.out.println(s.lineChars);
+       
+        Tokenizer tokenizer = new Tokenizer("C:/Christopher/School/OSU/AU21/CSE3341/Project/Tokenizer/src/test.txt");
+        int token = 0;
+        while((token = tokenizer.getToken()) != 33 || token != -1){
+            System.out.println(token);
+            tokenizer.skipToken();
+        }
     }
 }
--- src/Tokenizer.java
+++ src/Tokenizer.java
@@ -2,17 +2,78 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PushbackReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class Tokenizer {
+    
+    // Class variables 
+    public int token;
+    public PushbackReader pushbackReader;
+    ArrayList<String> keywords = new ArrayList<>(Arrays.asList("program","begin", "end", "int", "if", "then", "else", "while", "loop", "read", "write"));
+    ArrayList<String> specialCharacters = new ArrayList<String>(Arrays.asList(";",",","=","!","[","]","&&","||","(",")","+","-","*","!=","==","<",">","<=",">="));
+    Map<String,Integer> grammarTable = new HashMap<String, Integer>();
+    //--------------------------
 
-    private final Set<String> keywords = new HashSet<>(Arrays.asList("program", "begin", "end", "new", "int", "define", "endfunc", "class", "extends", "endclass",
-    "if", "then", "else", "while", "endwhile", "endif", "or", "input", "output", "ref"));
-
-    public static String getToken(ArrayList<String> tokens){
-        return "token";
+    //Constructor
+    Tokenizer(String fileName){
+        for(int i =0; i<keywords.size();i++){
+            this.grammarTable.put(keywords.get(i), i+1 );
+        }
+        // Add Special characters to grammar table
+        for(int j =0; j < specialCharacters.size(); j++){
+            this.grammarTable.put(specialCharacters.get(j), j+12 );
+        }
+        //---------
+        // Remaining grammar items
+        this.grammarTable.put("integer",31);
+        this.grammarTable.put("identifier", 32);
+        this.grammarTable.put("EOF", 33);
+        try{
+            this.pushbackReader = new PushbackReader(new FileReader(fileName)); 
+        }catch(Exception e){
+            System.out.println("ERROR USING BUFFER READER");
+            e.printStackTrace();
+        }
     }
 
-    public static void skipToken(){
+    // Main methods
+    public int getToken(){
+        return this.token;
+    }
+
+    public void skipToken() throws IOException{
+        int currentCharInt = this.pushbackReader.read();
+        char currentChar = (char)currentCharInt;
+
+        if(!Character.isWhitespace(currentChar)){
+            StringBuilder builder = new StringBuilder();
+            builder.append((char)currentCharInt);  
+
+
+            if(this.specialCharacters.contains("" + currentChar)){ 
+                if(currentChar =='&' || currentChar =='|' || currentChar=='<' || currentChar == '>' ||currentChar =='='){
+                    char nextChar = (char) this.pushbackReader.read();
+                    if((currentChar == '&' && nextChar == '&') || (currentChar == '|' && nextChar == '|') || (currentChar == '<' && nextChar =='=') || (currentChar == '>' && nextChar == '=') || (currentChar == '=' && nextChar == '=')){
+                        builder.append(nextChar);
+                        this.token = this.grammarTable.get(builder.toString());
+                    }
+                }else{
+                    this.token = this.grammarTable.get(builder.toString());
+                }
+            }else if(Character.isLetter(currentChar)){
+                char nextChar = (char) this.pushbackReader.read();
+                while(Character.isLetterOrDigit(nextChar)){
+                    builder.append(nextChar);
+                }
+            }
+        }
 
     }
 
@@ -22,25 +83,5 @@ public class Tokenizer {
 
     public static int idName(String token){
         return 1;
-    }
-
-    public void tokenize(ArrayList<Character> lineChars){
-        ArrayList<String> tokens = new ArrayList<>();
-        ArrayList<Character> token = new ArrayList<>();
-        for(int i =0; i <lineChars.size();i++){
-            char nextChar = lineChars.get(i);
-            if(!Character.isWhitespace(nextChar)){
-                token.add(nextChar);
-            }
-            else{
-                StringBuilder builder = new StringBuilder();
-                for(Character c : token){
-                    builder.append(c);
-                }
-                token.clear();
-                tokens.add(builder.toString());
-            }
-        }
-        System.out.println(tokens);
     }
 }
