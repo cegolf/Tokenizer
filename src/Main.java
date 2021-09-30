47b6549436e84d3491863114c2cbef73fe3e90a6
egolfc <cae5511@gmail.com>
2021-09-29 22:21:25 -0400

    working without command line

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) throws Exception {
        
-        Tokenizer tokenizer = new Tokenizer("C:/Christopher/School/OSU/AU21/CSE3341/Project/Tokenizer/src/test.txt");
+        Tokenizer tokenizer = new Tokenizer(args[1]);
         int token = tokenizer.getToken();
         while(token != 33 && token != -1){
             System.out.println(token);
@@ -11,7 +11,7 @@ public class Main {
         if(token == -1){
             System.out.println("INVALID TOKEN");
         }else{
-            System.out.println("END OF FILE");
+            System.out.println(token);
         }
     }
 }
